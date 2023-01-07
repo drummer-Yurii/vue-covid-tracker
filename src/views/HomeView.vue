aa98b2f29fc5e326db42741d4aedde6e1e846178
Yurii Rybachok <drummer2009@ukr.net>
2023-01-07 16:16:20 +0200

    data boxes component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <main v-if="!loading">
     <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -12,10 +13,12 @@
 
 <script>
 import DataTitle from '@/components/DataTitle.vue'
+import DataBoxes from '@/components/DataBoxes.vue'
 export default {
   name: 'HomeView',
   components: {
-    DataTitle
+    DataTitle,
+    DataBoxes
   },
   data() {
     return {
